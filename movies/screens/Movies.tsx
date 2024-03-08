3dfcf9f5c12247bf5beb41f5926da47d066ecd82
k-oyun <dhdbs001208@naver.com>
2024-03-08 15:49:43 +0900

    Finish Comming Soom part

--- movies/screens/Movies.tsx
+++ movies/screens/Movies.tsx
@@ -52,6 +52,36 @@ const Votes = styled.Text`
   font-size: 10px;
 `;
 
+const ListContainer = styled.View`
+  margin-bottom: 40px;
+`;
+
+const HMovie = styled.View`
+  padding: 0px 30px;
+  flex-direction: row;
+  margin-bottom: 30px;
+`;
+
+const HColumn = styled.View`
+  margin-left: 15px;
+  width: 80%;
+`;
+
+const Overview = styled.Text`
+  color: white;
+  opacity: 0.7;
+  width: 80%;
+`;
+
+const Release = styled.Text`
+  color: white;
+  font-size: 12px;
+  margin-vertical: 10px;
+`;
+
+const CommingSoonTitle = styled(ListTitle)`
+  margin-bottom: 10px;
+`;
 //화면의 높이를 알려줌 : dimension
 const {height: SCREEN_HEIGHT} = Dimensions.get("window");
 
@@ -134,25 +164,53 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
           />
         ))}
       </Swiper>
-      <ListTitle>Trending movie</ListTitle>
-      <TrendingScroll
-        contentContainerStyle={{paddingLeft: 30}}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-      >
-        {trending.map((movie) => (
-          <Movie key={movie.id}>
-            <Poster path={movie.poster_path} />
-            {/* 제목이 긴 영화는 13까지만 */}
-            {/* 13보다 길면 ...로 대체 */}
-            <Title>
-              {movie.original_title.slice(0, 13)}
-              {movie.original_title.length > 13 ? "..." : null}
-            </Title>
-            <Votes>⭐️{movie.vote_average}</Votes>
-          </Movie>
-        ))}
-      </TrendingScroll>
+      <ListContainer>
+        <ListTitle>Trending movie</ListTitle>
+        <TrendingScroll
+          contentContainerStyle={{paddingLeft: 30}}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          {trending.map((movie) => (
+            <Movie key={movie.id}>
+              <Poster path={movie.poster_path} />
+              {/* 제목이 긴 영화는 13까지만 */}
+              {/* 13보다 길면 ...로 대체 */}
+              <Title>
+                {movie.original_title.slice(0, 13)}
+                {movie.original_title.length > 13 ? "..." : null}
+              </Title>
+
+              <Votes>
+                {movie.vote_average > 0
+                  ? `⭐️${movie.vote_average}/10`
+                  : `Comming soon`}
+              </Votes>
+            </Movie>
+          ))}
+        </TrendingScroll>
+      </ListContainer>
+      <CommingSoonTitle>Comming Soon</CommingSoonTitle>
+      {upcoming.map((movie) => (
+        <HMovie key={movie.id}>
+          <Poster path={movie.poster_path} />
+          <HColumn>
+            <Title>{movie.original_title}</Title>
+            <Release>
+              {new Date(movie.release_date).toLocaleDateString("ko", {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+              })}
+            </Release>
+            <Overview>
+              {movie.overview !== "" && movie.overview.length > 80
+                ? `${movie.overview.slice(0, 140)}...`
+                : movie.overview}
+            </Overview>
+          </HColumn>
+        </HMovie>
+      ))}
     </Container>
   );
 };
