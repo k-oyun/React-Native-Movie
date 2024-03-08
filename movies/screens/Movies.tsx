6628a64683c13e64839d17910c4876bb15c6dde3
k-oyun <dhdbs001208@naver.com>
2024-03-08 13:01:39 +0900

    movie screen part three

--- movies/screens/Movies.tsx
+++ movies/screens/Movies.tsx
@@ -1,11 +1,15 @@
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
-import {join} from "path";
 import React, {useEffect, useState} from "react";
 import {BlurView} from "expo-blur";
-import {ActivityIndicator, Dimensions, StyleSheet} from "react-native";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
 
 //swiper를 이용하면 스크롤뷰와 달리 자동으로 스크롤이되는 효과를 가지고 있다
-import Swiper from "react-native-web-swiper";
+import Swiper from "react-native-swiper";
 import styled from "styled-components/native";
 import {makeImgPath} from "../utils";
 
@@ -25,11 +29,45 @@ const Loader = styled.View`
 
 const BgImg = styled.Image``;
 
-const Title = styled.Text``;
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`;
+
+const Title = styled.Text`
+  font-weight: 600;
+  font-size: 16px;
+  color: white;
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Column = styled.View`
+  width: 40%;
+  margin-left: 15px;
+`;
+
+const Overview = styled.Text`
+  margin-top: 10;
+  color: rgba(255, 255, 255, 0.8);
+`;
+
+//다른 요소의 style을 가져옴 : extend style
+const Votes = styled(Overview)`
+  font-size: 12px;
+`;
+
 //화면의 높이를 알려줌 : dimension
 const {height: SCREEN_HEIGHT} = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
 
@@ -55,12 +93,15 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   ) : (
     <Container>
       <Swiper
+        horizontal
         //반복
         loop
         //몇초마다 반복할지
-        timeout={3.5}
+        autoplay
+        autoplayTimeout={3.5}
         //스크롤을 하지 못하도록
-        controlsEnabled={false}
+        showsButtons={false}
+        showsPagination={false}
         containerStyle={{width: "100%", height: SCREEN_HEIGHT / 4}}
       >
         {nowPlaying.map((movie) => (
@@ -69,8 +110,24 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
               style={StyleSheet.absoluteFill}
               source={{uri: makeImgPath(movie.backdrop_path)}}
             />
-            <BlurView intensity={80} style={StyleSheet.absoluteFill}>
-              <Title>{movie.original_title}</Title>
+            <BlurView
+              tint={isDark ? "dark" : "light"}
+              intensity={80}
+              style={StyleSheet.absoluteFill}
+            >
+              {/* uri : 웹에 이쓴ㄴ 이미지를 불러오는 방법 */}
+              <Wrapper>
+                <Poster source={{uri: makeImgPath(movie.poster_path)}} />
+                <Column>
+                  <Title>{movie.original_title}</Title>
+                  {/* overview가 너무 긴 경우에는 잘라야함 */}
+                  {/* 80까지 자름 */}
+                  <Overview>{movie.overview.slice(0, 90)}...</Overview>
+                  {movie.vote_average > 0 ? (
+                    <Votes>⭐️{movie.vote_average}/10</Votes>
+                  ) : null}
+                </Column>
+              </Wrapper>
             </BlurView>
           </View>
         ))}
